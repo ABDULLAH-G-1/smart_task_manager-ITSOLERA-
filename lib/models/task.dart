f949b75e8f1ff94fafaefe3ff5ca4847f64c936f
ABDULLAH-G-1 <abdullah.abdulghafoor44@gmail.com>
2026-01-18 12:17:02 +0500

    minor

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,25 +1,24 @@
 import 'package:hive/hive.dart';
 
-// Ye line error degi jab tak hum generator command nahi chalayenge.
-// Isay filhal ignore karna.
+
 part 'task.g.dart';
 
 @HiveType(typeId: 0)
 class Task extends HiveObject {
   @HiveField(0)
-  String title;       // Task ka naam
+  String title;       // Task name
 
   @HiveField(1)
-  String description; // Details (Long text handling ke liye)
+  String description; // Details (for Long text handling )
 
   @HiveField(2)
-  bool isCompleted;   // Completed ya Pending
+  bool isCompleted;   // Completed or Pending
 
   @HiveField(3)
-  DateTime createdAt; // Sorting ke liye (Newest first)
+  DateTime createdAt; // Sorting  (Newest first)
 
   @HiveField(4)
-  String priority;    // Bonus: 'Low', 'Medium', 'High'
+  String priority;    // perority: 'Low', 'Medium', 'High'
 
   Task({
     required this.title,
